2adc1e493f7bcc0a3da28762b36102546748df3f
patrick <patrickwuke@163.com>
2020-10-07 23:45:18 +0800

    close

--- app/maven/maven_endpoints.py
+++ app/maven/maven_endpoints.py
@@ -9,7 +9,9 @@ from .services import get_artifact_info
 
 maven_router = APIRouter()
 
-
+"""
+get maven artifact detail information
+"""
 @maven_router.get("/maven", response_model=models.AllArtifactsResponse)
 def get_maven_repo(g: Optional[str] = None, a: Optional[str] = None, v: Optional[str] = None):
     if g is None and a is None:
